85ffb855c7167b3d62a80af2f799a572a1fae71e
EdgarMM19 <edgarmm19@gmail.com>
2021-03-30 21:35:41 +0200

    randomGenerator (permutation)

---
+++ randomGenerators.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include <random>
+
+using namespace std;
+
+std::random_device rd;
+    std::mt19937::result_type seed = rd() ^ (
+            (std::mt19937::result_type)
+            std::chrono::duration_cast<std::chrono::seconds>(
+                std::chrono::system_clock::now().time_since_epoch()
+                ).count() +
+            (std::mt19937::result_type)
+            std::chrono::duration_cast<std::chrono::microseconds>(
+                std::chrono::high_resolution_clock::now().time_since_epoch()
+                ).count() );
+
+std::mt19937 gen(seed);
+std::uniform_int_distribution<int> distrib(0, int((1LL<<32)-1));
+
+vector<int> generateRandomPermutation(int len, bool startAtOne){
+    //https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
+    //https://stackoverflow.com/questions/13445688/how-to-generate-a-random-number-in-c
+    
+    vector<int> v(len);
+    for(int i = 0; i < len; ++i)
+        v[i] = i + startAtOne;
+    for(int i = len-1; i >= 1; --i){
+        int j = (distrib(gen)%(i+1)+(i+1))%(i+1);
+        swap(v[i], v[j]);
+    }
+    return v;
+}
+int main(){
+    for(int i = 1; i < 10; ++i){
+        vector<int> v = generateRandomPermutation(i, 0);
+        for(auto x : v) cout << x << " ";
+        cout << endl;
+        v = generateRandomPermutation(i, 1);
+        for(auto x : v) cout << x << " ";
+        cout << endl;
+    }
+}
